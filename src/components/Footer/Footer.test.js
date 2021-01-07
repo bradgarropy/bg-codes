ba4947e2c57ecf8c68c040b070d908e5685a5a9d
Brad Garropy <bradgarropy@gmail.com>
2021-01-07 15:16:23 -0600

    remove footer.

--- src/components/Footer/Footer.test.js
+++
@@ -1,9 +0,0 @@
-import Footer from "components/Footer"
-import {render, screen} from "test-utils/render"
-
-test("shows footer", () => {
-    render(<Footer />)
-
-    expect(screen.getByText("FOOTER"))
-    expect(screen.getByText("built by"))
-})

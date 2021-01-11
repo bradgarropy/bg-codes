5edc09d19705774a581bcbb91ea5513ecc7adcd3
Brad Garropy <bradgarropy@gmail.com>
2021-01-11 10:52:41 -0600

    no redirect. just copy.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,34 @@
-import {navigate} from "gatsby"
+import Background from "components/Background"
+import styled from "styled-components"
 
-const IndexPage = () => {
-    navigate("/brb", {replace: true})
-    return null
+const Hero = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    display: grid;
+    justify-items: center;
+`
+
+const Tagline = styled.span`
+    text-transform: uppercase;
+    font-size: 12.5rem;
+    background-color: ${({theme}) => theme.colors.white};
+    padding: 0 4rem;
+    transform: skew(-3deg, -3deg);
+`
+
+const BRBPage = () => {
+    return (
+        <>
+            <Background />
+
+            <Hero>
+                <img src="/bg.png" alt="bg" />
+                <Tagline>codes</Tagline>
+            </Hero>
+        </>
+    )
 }
 
-export default IndexPage
+export default BRBPage

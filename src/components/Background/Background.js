4f31f9293b658cf0ab19d0def07a40da38262ae7
Brad Garropy <bradgarropy@gmail.com>
2021-01-11 13:47:06 -0600

    fix background.

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -1,6 +1,10 @@
 import styled from "styled-components"
 
 const BackgroundWrapper = styled.img`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
     width: 100%;
     height: 100%;
     object-fit: cover;

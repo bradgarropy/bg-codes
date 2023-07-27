f100ccee81901763ed0796461caa398f95674b6c
Brad Garropy <bgarropy@atlassian.com>
2023-07-27 14:39:37 -0500

    social link.

--- src/components/SocialLink/SocialLink.js
+++ src/components/SocialLink/SocialLink.js
@@ -1,23 +1,15 @@
 import Link from "@bradgarropy/gatsby-link"
 import Icon from "components/Icon"
 import PropTypes from "prop-types"
-import styled from "styled-components"
-
-const SocialLinkWrapper = styled(Link)`
-    height: 40px;
-    color: ${({theme}) => theme.colors.black};
-    transition: all 300ms ease-out;
-
-    &:hover {
-        color: ${({theme}) => theme.colors.purple};
-    }
-`
 
 const SocialLink = ({platform}) => {
     return (
-        <SocialLinkWrapper to={`https://${platform}.com/bradgarropy`}>
+        <Link
+            className="h-[40px] text-black transition-colors duration-300 ease-out hover:text-[#a038ff]"
+            to={`https://${platform}.com/bradgarropy`}
+        >
             <Icon icon={platform} />
-        </SocialLinkWrapper>
+        </Link>
     )
 }
 
